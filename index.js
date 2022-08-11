e27c02491af10caf38b9902f5e190e4b8930005c
caseyjramirez <caseyjramirez5@gmail.com>
2022-08-11 08:53:38 -0500

    commit tree b0dd21f1b40d52962ff225697691b6b2bfaf9a5b

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+function eventHander() {
+    console.log('clicked');
 }
+
+function addingEventListener() {
+    const btn = document.getElementById('button')
+    btn.addEventListener('click', eventHander)
+
+}
+
+addingEventListener()
